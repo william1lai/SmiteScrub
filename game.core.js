9a6506657622d3b71f80fa9d2595300315f40d20
William Lai <williamlai@ucla.edu>
2015-03-23 11:52:19 -0700

    stuff5

--- game.core.js
+++ game.core.js
@@ -38,6 +38,7 @@ if('undefined' != typeof(global)) frame_time = 45; //on server we run at 45ms, 2
             width : 720,
             height : 480
         };
+        this.input_seq = 0;
         this.hp = 5000;
         this.smite_dmg = 1000;
         this.decay = 10;
